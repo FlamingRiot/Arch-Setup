ee8932b24d107d6e01dbbf53abbdf65d5d186398
Evan Comtesse <comtesseevan@gmail.com>
2025-06-10 23:18:39 +0200

    feat: added installations to the original script

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 # System update
 echo "Updating system..."
 sudo pacman -Syu --noconfirm
+sudo usermod -aG input,seat $USER # Some weird-ass command to enable peripheric detection
 
 # Hyprland installation
 echo "Installing hyprland..."
@@ -19,3 +20,22 @@ makepkg -si
 echo "Installing Brave..."
 curl -fsS https://dl.brave.com/install.sh | sh
 
+# Vscodium installation
+echo "Installing Vscodium..."
+yay -S --noconfirm --clean vscodium-bin
+
+# Emojis
+echo "Installing emojis..."
+yay -S --noconfirm --clean ttf-twemoji
+
+# Rofi (fixed wayland version)
+echo "Installing rofi..."
+yay -S --noconfirm --clean rofi-lbonn-wayland 
+
+# Dolphin file explorer
+echo "Installing dolphin..."
+sudo pacman -S dolphin
+
+# Waybar and its dependencies
+sudo pacman -S waybar
+sudo pacman -S ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-common
