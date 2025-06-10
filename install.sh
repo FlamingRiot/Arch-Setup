96c1d1a079906f8f981eac403ff3049a1cf64cf8
Evan Comtesse <comtesseevan@gmail.com>
2025-06-10 22:35:42 +0200

    feat: added hyprland, brave and yay installation to install script

--- install.sh
+++ install.sh
@@ -5,3 +5,17 @@ echo "Updating system..."
 sudo pacman -Syu --noconfirm
 
 # Hyprland installation
+echo "Installing hyprland..."
+sudo pacman -S hyprland wayland wayland-protocols xorg-xwayland kitty sddm git
+sudo systemctl enable sddm.service
+
+# AUR (yay) Installation
+echo "Installing yay..."
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+
+# Brave Installation
+echo "Installing Brave..."
+curl -fsS https://dl.brave.com/install.sh | sh
+
