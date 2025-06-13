eb8397b5e563d0649451947d55e5d9a9219ea5c4
Evan <comtesseevan@gmail.com>
2025-06-13 14:59:03 +0200

    fix: reset colors after echo in install.sh script

--- install.sh
+++ install.sh
@@ -1,63 +1,63 @@
 # Installation script for Hyprland along with a personal configuration
 
 # System update
-echo -e "\e[92mUpdating system..."
+echo -e "\e[92mUpdating system...\e[m"
 sudo pacman -Syu --noconfirm
 sudo usermod -aG input,seat $USER # Some weird-ass command to enable peripheric detection
 
 # Nvidia pilotes installation
-echo -e "\e[92mInstalling Nvidia pilots..."
+echo -e "\e[92mInstalling Nvidia pilots...\e[m"
 sudo pacman -Sy nvidia nvidia-utils nvidia-settings
 echo "nvidia" | sudo tee /etc/modules-load.d/nvidia.conf
 sudo mkinitcpio -P
 sudo pacman -Sy nvidia-prime # Used for running apps on the Nvidia GPU
 
 # Hyprland installation
-echo -e "\e[92mInstalling hyprland..."
+echo -e "\e[92mInstalling hyprland...\e[m"
 sudo pacman -Sy hyprland wayland wayland-protocols xorg-xwayland kitty sddm git
 sudo systemctl enable sddm
 
 # AUR (yay) Installation
-echo -e "\e[92mInstalling yay..."
+echo -e "\e[92mInstalling yay...\e[m"
 exit 
 git clone https://aur.archlinux.org/yay.git
 cd yay
 su - "$SUDO_USER" -c "makepkg -si"
 
 # Brave Installation
-echo -e "\e[92mInstalling Brave..."
+echo -e "\e[92mInstalling Brave...\e[m"
 curl -fsS https://dl.brave.com/install.sh | sh
 
 # Vscodium installation
-echo -e "\e[92mInstalling Vscodium..."
+echo -e "\e[92mInstalling Vscodium...\e[m"
 yay -S --noconfirm --clean vscodium-bin
 
 # Emojis
-echo -e "\e[92mInstalling emojis..."
+echo -e "\e[92mInstalling emojis...\e[m"
 yay -S --noconfirm --clean ttf-twemoji
 
 # Rofi (fixed wayland version)
-echo -e "\e[92mInstalling rofi..."
+echo -e "\e[92mInstalling rofi...\e[m"
 yay -S --noconfirm --clean rofi-lbonn-wayland 
 
 # Dolphin file explorer
-echo -e "\e[92mInstalling dolphin..."
+echo -e "\e[92mInstalling dolphin...\e[m"
 sudo pacman -Sy dolphin
 
 # Waybar and its dependencies
-echo -e "\e[92mInstalling waybar..."
+echo -e "\e[92mInstalling waybar...\e[m"
 sudo pacman -Sy waybar
 sudo pacman -Sy ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-common
 
 # Hyprpaper 
-echo -e "\e[92mInstalling and configuring hyprpaper..."
+echo -e "\e[92mInstalling and configuring hyprpaper...\e[m"
 sudo pacman -Sy hyprpaper
 mkdir ~/.config/hypr/wallpapers
 cp wallpapers/YourName.jpg ~/.config/hypr/wallpapers
 
 
 # Copying configs
-echo -e "\e[92mCopying configuration files..."
+echo -e "\e[92mCopying configuration files...\e[m"
 cp -rf hyprland ../.config/hypr/hyprland
 cp -rf kitty ../.config/kitty
 cp -rf wallpapers ../.config/hypr/wallpapers
@@ -69,5 +69,5 @@ cp -f .bashrc ../.bashrc
 cp ascii.txt ../.config/ascii.txt
 
 # Reboot system
-echo -e "\e[92mRebooting system..."
+echo -e "\e[92mRebooting system...\e[m"
 reboot
