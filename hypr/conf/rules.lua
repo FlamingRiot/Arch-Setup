f3436354e1a0ce6cfcac2a86cc16d805566ab8e9
Evan <comtesseevan@gmail.com>
2026-05-17 14:35:04 +0200

    feat: added support for hyprlock + anim features

--- hypr/conf/rules.lua
+++ hypr/conf/rules.lua
@@ -8,6 +8,15 @@
 -- See https://wiki.hypr.land/Configuring/Basics/Window-Rules/
 -- and https://wiki.hypr.land/Configuring/Basics/Workspace-Rules/
 
+hl.window_rule({
+    name = "floating-terminals",
+    match = {
+        class = "kitty"
+    },
+    float = true,
+    size = {"(monitor_w*0.6)", "(monitor_h*0.6)"}
+})
+
 -- Example window rules that are useful
 
 local suppressMaximizeRule = hl.window_rule({
